840f0239fdb37a5657b2dd015b16701fd93865eb
man2001thcs <82664793+man2001thcs@users.noreply.github.com>
2022-04-24 10:15:50 +0700

    Add files via upload

---
+++ Lab02/exp-2/day.php
@@ -0,0 +1,37 @@
+<HTML>
+    <Head>
+        <title>
+            A simple form
+</title>
+</head>
+<body>
+    <form action="/dayR.php" method="post">
+        Enter your information here
+        <br>
+        <label style="color: blue; display: block;">
+            Name
+        </label>
+        <input type="text" name="Name">
+        <br>
+        <label style="color: blue; display: block;">
+            Email
+        </label>
+        <input type="text" name="email">
+        <br>
+        
+        <label for="day">Date:</label>
+        <input type="date" id="day" name="day">
+        <br>
+
+        <label for="times">Time:</label>
+        <input type="time" id="times" name="times">
+        <br>
+        
+        
+        <input type="submit" value="Click to submit">
+        <input type="reset" name="Restart">
+    </form>
+
+</body>
+</html>
+
